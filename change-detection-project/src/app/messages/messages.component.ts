67cbfd942285bc763770cd3fd270a27a8952cb2a
Siarhei B <sergeybednyuk@gmail.com>
2026-03-25 15:50:36 +0100

    feat[change-detection-project]: implement OnPush strategy

--- change-detection-project/src/app/messages/messages.component.ts
+++ change-detection-project/src/app/messages/messages.component.ts
@@ -1,24 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 import { MessagesListComponent } from './messages-list/messages-list.component';
 import { NewMessageComponent } from './new-message/new-message.component';
 
 @Component({
-  selector: 'app-messages',
-  standalone: true,
-  templateUrl: './messages.component.html',
-  styleUrl: './messages.component.css',
-  imports: [MessagesListComponent, NewMessageComponent],
+    selector: 'app-messages',
+    standalone: true,
+    templateUrl: './messages.component.html',
+    styleUrl: './messages.component.css',
+    imports: [MessagesListComponent, NewMessageComponent],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MessagesComponent {
-  messages = signal<string[]>([]);
-
-  get debugOutput() {
-    console.log('[Messages] "debugOutput" binding re-evaluated.');
-    return 'Messages Component Debug Output';
-  }
-
-  onAddMessage(message: string) {
-    this.messages.update((oldMessages) => [...oldMessages, message]);
-  }
+    get debugOutput() {
+        console.log('[Messages] "debugOutput" binding re-evaluated.');
+        return 'Messages Component Debug Output';
+    }
 }
