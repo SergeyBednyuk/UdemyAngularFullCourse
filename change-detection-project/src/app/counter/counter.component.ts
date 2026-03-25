dc39ec2fc60a1c79047b3d6ccad08fd6011b4bbf
Siarhei B <sergeybednyuk@gmail.com>
2026-03-25 15:11:03 +0100

    refactor[change-detection-project.counter]: turn of zone detection

--- change-detection-project/src/app/counter/counter.component.ts
+++ change-detection-project/src/app/counter/counter.component.ts
@@ -1,27 +1,40 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, NgZone, OnInit, signal } from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
 @Component({
-  selector: 'app-counter',
-  standalone: true,
-  templateUrl: './counter.component.html',
-  styleUrl: './counter.component.css',
-  imports: [InfoMessageComponent],
+    selector: 'app-counter',
+    standalone: true,
+    templateUrl: './counter.component.html',
+    styleUrl: './counter.component.css',
+    imports: [InfoMessageComponent],
 })
-export class CounterComponent {
-  count = signal(0);
+export class CounterComponent implements OnInit {
+    private zone = inject(NgZone);
+    count = signal(0);
 
-  get debugOutput() {
-    console.log('[Counter] "debugOutput" binding re-evaluated.');
-    return 'Counter Component Debug Output';
-  }
+    get debugOutput() {
+        console.log('[Counter] "debugOutput" binding re-evaluated.');
+        return 'Counter Component Debug Output';
+    }
 
-  onDecrement() {
-    this.count.update((prevCount) => prevCount - 1);
-  }
+    ngOnInit(): void {
+        setTimeout(() => {
+            this.count.set(0);
+        }, 4000);
 
-  onIncrement() {
-    this.count.update((prevCount) => prevCount + 1);
-  }
+        this.zone.runOutsideAngular(() => {
+            setTimeout(() => {
+                console.log('TIMER EXPIRED!');
+            }, 5000);
+        });
+    }
+
+    onDecrement() {
+        this.count.update((prevCount) => prevCount - 1);
+    }
+
+    onIncrement() {
+        this.count.update((prevCount) => prevCount + 1);
+    }
 }
