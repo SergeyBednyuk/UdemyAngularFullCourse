08430c8ca5a38268527a36bd466f26ef7239e39d
Siarhei B <sergeybednyuk@gmail.com>
2026-04-01 15:07:01 +0200

    feat[template-driven-reactive-project.login]: add an ability to save entered email to localsotrage

--- template-driven-reactive-project/src/app/auth/login/login.component.ts
+++ template-driven-reactive-project/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
 import { FormsModule, NgForm } from "@angular/forms";
+import { debounceTime } from "rxjs";
 
 @Component({
     selector: 'app-login',
@@ -11,17 +12,44 @@ import { FormsModule, NgForm } from "@angular/forms";
     ]
 })
 export class LoginComponent {
+    private form = viewChild.required<NgForm>('form');
+    private destroyRef = inject(DestroyRef);
+
+    constructor() {
+        const savedForm = window.localStorage.getItem('saved-login-form');
+
+        if (savedForm) {
+            const loadedFormData = JSON.parse(savedForm);
+            const savedEmail = loadedFormData.email;
+            setTimeout(() => {
+                this.form().controls['emailInpt'].setValue(savedEmail);
+            }, 5);
+        }
+
+        afterNextRender(() => {
+            const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
+                next: (value) =>
+                    window.localStorage.setItem('saved-login-form', JSON.stringify({ email: value.emailInpt }))
+            });
+
+            this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+        });
+    }
+
+
     onSubmit(formData: NgForm) {
-        if (formData.form.invalid ){
+        if (formData.form.invalid) {
             return;
         }
 
-        const enteredEmail =  formData.form.value.emailInpt;
-        const enteredPassword =  formData.form.value.psswrdInpt;
+        const enteredEmail = formData.form.value.emailInpt;
+        const enteredPassword = formData.form.value.psswrdInpt;
 
         console.log(formData.form.status);
         console.log(formData.form);
         console.log('EMAIL:' + enteredEmail);
         console.log('PASSWORD:' + enteredPassword);
+
+        formData.form.reset();
     }
 }
