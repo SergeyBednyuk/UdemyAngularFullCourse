5ed199e08eff8b83e98995d4f6f1744ee3d58225
Siarhei B <sergeybednyuk@gmail.com>
2026-04-01 14:29:32 +0200

    feat[template-driven-reactive-project.login]: add default validation to the form

--- template-driven-reactive-project/src/app/auth/login/login.component.ts
+++ template-driven-reactive-project/src/app/auth/login/login.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+    selector: 'app-login',
+    standalone: true,
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.css',
+    imports: [
+        FormsModule
+    ]
 })
-export class LoginComponent {}
+export class LoginComponent {
+    onSubmit(formData: NgForm) {
+        if (formData.form.invalid ){
+            return;
+        }
+
+        const enteredEmail =  formData.form.value.emailInpt;
+        const enteredPassword =  formData.form.value.psswrdInpt;
+
+        console.log(formData.form.status);
+        console.log(formData.form);
+        console.log('EMAIL:' + enteredEmail);
+        console.log('PASSWORD:' + enteredPassword);
+    }
+}
