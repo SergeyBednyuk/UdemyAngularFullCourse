fb2e8f7970c5a83e45a9776d931842c8b2e55ea1
Siarhei B <sergeybednyuk@gmail.com>
2026-04-10 18:31:47 +0200

    feat[lazy-loading-deferrable-views.app]: add Lazy-loading for users childs

--- lazy-loading-deferrable-views/src/app/app.routes.ts
+++ lazy-loading-deferrable-views/src/app/app.routes.ts
@@ -4,44 +4,44 @@ import { inject } from '@angular/core';
 import { routes as userRoutes } from './users/users.routes';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import {
-  UserTasksComponent,
-  resolveTitle,
-  resolveUserName,
+    UserTasksComponent,
+    resolveTitle,
+    resolveUserName,
 } from './users/user-tasks/user-tasks.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const dummyCanMatch: CanMatchFn = (route, segments) => {
-  const router = inject(Router);
-  const shouldGetAccess = Math.random();
-  if (shouldGetAccess < 1) {
-    return true;
-  }
-  return new RedirectCommand(router.parseUrl('/unauthorized'));
+    const router = inject(Router);
+    const shouldGetAccess = Math.random();
+    if (shouldGetAccess < 1) {
+        return true;
+    }
+    return new RedirectCommand(router.parseUrl('/unauthorized'));
 };
 
 export const routes: Routes = [
-  {
-    path: '', // <your-domain>/
-    component: NoTaskComponent,
-    // redirectTo: '/users/u1',
-    // pathMatch: 'full'
-    title: 'No task selected',
-  },
-  {
-    path: 'users/:userId', // <your-domain>/users/<uid>
-    component: UserTasksComponent,
-    children: userRoutes,
-    canMatch: [dummyCanMatch],
-    data: {
-      message: 'Hello!',
+    {
+        path: '', // <your-domain>/
+        component: NoTaskComponent,
+        // redirectTo: '/users/u1',
+        // pathMatch: 'full'
+        title: 'No task selected',
     },
-    resolve: {
-      userName: resolveUserName,
+    {
+        path: 'users/:userId', // <your-domain>/users/<uid>
+        component: UserTasksComponent,
+        loadChildren: () => import('./users/users.routes').then((m) => m.routes),
+        canMatch: [dummyCanMatch],
+        data: {
+            message: 'Hello!',
+        },
+        resolve: {
+            userName: resolveUserName,
+        },
+        title: resolveTitle,
     },
-    title: resolveTitle,
-  },
-  {
-    path: '**',
-    component: NotFoundComponent,
-  },
+    {
+        path: '**',
+        component: NotFoundComponent,
+    },
 ];
--- lazy-loading-deferrable-views/src/app/tasks/tasks.component.ts
+++ lazy-loading-deferrable-views/src/app/tasks/tasks.component.ts
@@ -6,8 +6,8 @@ import {
 import { ResolveFn, RouterLink } from '@angular/router';
 
 import { TaskComponent } from './task/task.component';
-import { TasksService } from './tasks.service';
 import { Task } from './task/task.model';
+import { TasksService } from "./tasks.service";
 
 @Component({
   selector: 'app-tasks',
@@ -23,22 +23,22 @@ export class TasksComponent {
 }
 
 export const resolveUserTasks: ResolveFn<Task[]> = (
-  activatedRouteSnapshot,
-  routerState
+    activatedRouteSnapshot,
+    routerState
 ) => {
-  const order = activatedRouteSnapshot.queryParams['order'];
-  const tasksService = inject(TasksService);
-  const tasks = tasksService
-    .allTasks()
-    .filter(
-      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
-    );
+    const order = activatedRouteSnapshot.queryParams['order'];
+    const tasksService = inject(TasksService);
+    const tasks = tasksService
+        .allTasks()
+        .filter(
+            (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
+        );
 
-  if (order && order === 'asc') {
-    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
-  } else {
-    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
-  }
+    if (order && order === 'asc') {
+        tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
+    } else {
+        tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
+    }
 
-  return tasks.length ? tasks : [];
+    return tasks.length ? tasks : [];
 };
